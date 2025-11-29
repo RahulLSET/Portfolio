3b5ffce4a417462c19099cd451d36eab1eb799bb
Rahul Shaw <rahulshaw@AntzDevicess-MacBook-Air-5.local>
2025-11-29 15:19:54 +0530

    Testing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
+  output: "standalone",
+
+  // 🟢 Prevent caching old build (very important for CI/CD)
+  poweredByHeader: false,
+  generateEtags: false,
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        { key: "Cache-Control", value: "no-store, no-cache, must-revalidate, max-age=0" }
+      ],
+    },
+  ],
 };
 
-module.exports = nextConfig;
+export default nextConfig;
